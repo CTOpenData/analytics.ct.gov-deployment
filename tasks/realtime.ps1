353fbbe4a7aa39b41566839a01db2b999c3c36f0
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-07-29 15:08:13 -0400

    Parameterize and abstract similar tasks into singular task script, utilize parameters across all scripts to avoid exposing file structure and solve scheduler default run location

--- tasks/realtime.ps1
+++ tasks/realtime.ps1
@@ -1,9 +1,14 @@
+# TO BE DELETE -- keeping for testing until we figure out remote execution issues
+
 # Set up location for scheduler to correctly read from
 param (
-  [Parameter(Mandatory=$true)][string]$Location
+  [Parameter(Mandatory=$true)][string]$ProjectLocation,
+  [Parameter(Mandatory=$true)][string]$OutputDirectory,
+  [Parameter(Mandatory=$true)][string]$Frequency,
 )
+
 # Run the analytics cli
 # . $PWD\env.ps1
-$file = "$LOCATION\analytics\test_for_env"
+$file = "$ProjectLocation\test_for_env"
 Add-Content $file "hello world lets add more data "
 # npx analytics --frequency realtime --slim --verbose --output $LOCATION\analytics\data
