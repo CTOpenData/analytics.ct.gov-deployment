9b645b96bca4a953c26ae5e5ccbad485c258032b
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-07-30 15:57:26 -0500

    Add remote execution to deploy reports
    Co-authored-by: Cailyn Hansen <cailyn.e.hansen@gmail.com>

--- tasks/create_tasks.ps1
+++ tasks/create_tasks.ps1
@@ -1,14 +1,20 @@
 # Set up location for scheduler to correctly read from
 param (
   [Parameter(Mandatory=$true)][string]$ProjectLocation,
-  [Parameter(Mandatory=$true)][string]$OutputDirectory,
+  [Parameter(Mandatory=$true)][string]$OutputDirectory
 )
+$CurrentLocation = Get-Location
+
+POWERSHELL $CurrentLocation$ProjectLocation\task.ps1 -ProjectLocation $CurrentLocation$ProjectLocation -OutputDirectory $CurrentLocation$OutputDirectory -Frequency realtime
+
+POWERSHELL "$CurrentLocation$ProjectLocation\teardown_tasks.ps1"
+
 # Realtime task (every minute)
 & "SCHTASKS" `
 /Create `
 /SC MINUTE `
 /MO 1 `
-/TR "POWERSHELL $ProjectLocation\analytics\task.ps1 -ProjectLocation $ProjectLocation -OutputDirectory $OutputDirectory -Frequency realtime" `
+/TR "POWERSHELL $CurrentLocation$ProjectLocation\task.ps1 -ProjectLocation $CurrentLocation$ProjectLocation -OutputDirectory $CurrentLocation$OutputDirectory -Frequency realtime" `
 /TN Analytics-Realtime
 
 # Hourly tasks
@@ -16,13 +22,13 @@ param (
 /Create `
 /SC HOURLY `
 /MO 1 `
-/TR "POWERSHELL $ProjectLocation\analytics\task.ps1 -ProjectLocation $ProjectLocation -OutputDirectory $OutputDirectory -Frequency hourly" `
-/TN Analytics-Daily
+/TR "POWERSHELL $CurrentLocation$ProjectLocation\task.ps1 -ProjectLocation $CurrentLocation$ProjectLocation -OutputDirectory $CurrentLocation$OutputDirectory -Frequency hourly" `
+/TN Analytics-Hourly
 
 # Daily tasks
 & "SCHTASKS" `
 /Create `
 /SC DAILY `
 /MO 1 `
-/TR "POWERSHELL $ProjectLocation\analytics\task.ps1 -ProjectLocation $ProjectLocation -OutputDirectory $OutputDirectory -Frequency daily" `
+/TR "POWERSHELL $CurrentLocation$ProjectLocation\task.ps1 -ProjectLocation $CurrentLocation$ProjectLocation -OutputDirectory $CurrentLocation$OutputDirectory -Frequency daily" `
 /TN Analytics-Daily
